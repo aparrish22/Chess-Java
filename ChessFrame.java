b36187fde96d1fc935bbe4d81cee935b963a8b07
aparrish22 <austinparrish202@gmail.com>
2022-12-26 01:27:43 -0600

    Created Chess Grid

--- ChessFrame.java
+++ ChessFrame.java
@@ -4,11 +4,92 @@ import java.awt.event.*;
 
 public class ChessFrame extends JFrame implements WindowListener, ActionListener {
 
-    Button newGameButton, loadButton, saveButton, 
+    JButton newGameButton, loadButton, saveButton, exitButton;
+
+    public ChessFrame() {
+
+        super();
+        setSize(400, 400);
+        setLayout(new FlowLayout());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        addWindowListener(this);
+
+
+        setPreferredSize(new Dimension(600, 600));
+
+        // buttons
+        newGameButton = new JButton(new AbstractAction("new game") {
+            @Override
+            public void actionPerformed( ActionEvent e ) {
+                drawBoard();
+            }
+        });
+
+        loadButton = new JButton(new AbstractAction("load game") {
+            @Override
+            public void actionPerformed( ActionEvent e ) {
+                // code here
+            }
+        });
+
+        saveButton = new JButton(new AbstractAction("save game") {
+            @Override
+            public void actionPerformed( ActionEvent e ) {
+                // code here
+            }
+        });
+
+        add(newGameButton);
+        add(loadButton);
+        add(saveButton);
+
+        pack();
+
+        newGameButton.addActionListener(this);
+        loadButton.addActionListener(this);
+        saveButton.addActionListener(this);
+
+        setVisible(true);
+
+    }
 
     public void actionPerformed(ActionEvent e) {
 
+    }
 
+    private void drawBoard() {
+
+        // Create the JPanel
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(500, 500));
+        panel.setLayout(new GridLayout(8, 8));
+
+        Color color;
+
+        // Create the board squares
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                boolean isLight = (row % 2) == (col % 2);
+
+                // Determine the background color for the square
+                color = (isLight)? Color.WHITE : Color.BLACK;
+
+                // Create the square and set its properties
+                JLabel square = new JLabel();
+                square.setPreferredSize(new Dimension(40, 40));
+                square.setOpaque(true);
+                square.setBackground(color);
+
+                // Add the square to the panel
+                panel.add(square);
+            }
+
+            
+        }
+        
+        add(panel);
+        setVisible(true);
 
     }
 
--- Piece.java
+++ Piece.java
@@ -8,9 +8,21 @@ public abstract class Piece {
         return isCaptured;
     }
 
+    public void setName(String s) {
+        shortName = s;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setIsCaptured(boolean b) {
+        isCaptured = b;
+    }
     
     abstract String capture(String pos);
     abstract String move(String pos);
+    abstract boolean isLegalMove();
     abstract String recordMove(String move);
     abstract boolean promotable(); // promote
 
